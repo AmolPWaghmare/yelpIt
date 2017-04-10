7bf51b3d620068dd24f6eaa1ba4961759370f0af
Waghmare, Amol <Amol_Waghmare@intuit.com>
2017-04-09 21:26:53 -0700

    Updated Readme

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -21,7 +21,7 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
         
-        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+        Business.searchWithTerm(term: "Restaurants", completion: { (businesses: [Business]?, error: Error?) -> Void in
                 self.businesses = businesses
                 self.tableView.reloadData()
             }
